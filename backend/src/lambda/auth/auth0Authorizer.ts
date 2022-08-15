0876afbc5df0be6a55a3c773a3aab3daa4aa65e9
Pablo <pablomelo.inf@gmail.com>
2022-08-14 22:18:35 -0300

    add middy and implement getTodo

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,29 +1,33 @@
-import { CustomAuthorizerEvent, CustomAuthorizerResult,CustomAuthorizerHandler } from 'aws-lambda'
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS from 'aws-sdk'
 import { createLogger } from '../../utils/logger'
 import { verify } from 'jsonwebtoken'
 import { JwtToken } from '../../auth/JwtToken'
 //import { Jwt } from '../../auth/Jwt'
 
-//const auth0Secret = process.env.AUTH_0_SECRET
+
 const logger = createLogger('auth')
+
+import * as middy from 'middy'
+import {secretsManager} from 'middy/middlewares'
+
+
 const secretId = process.env.AUTH_0_SECRET_ID
 const secretField = process.env.AUTH_0_SECRET_FIELD
 
-const client = new AWS.SecretsManager()
-
-//Cache secret if a Lambda instance is reused
-let cachedSecret: string
-
-
-export const handler: CustomAuthorizerHandler =async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
+export const handler =  middy(
+ async (event: CustomAuthorizerEvent,
+        context    
+  ): Promise<CustomAuthorizerResult> => {
   
   // logger.info('authHeader a user', event)
   try{
-    const decodedToken  = await verifyToken(event.authorizationToken)
+    const decodedToken  = verifyToken(
+      event.authorizationToken,
+      context.AUTH0_SECRET[secretField]
+    )
     //console.log('User was authorized');
-    logger.info('decodedToken:', decodedToken)
+    logger.info('User was authorized', decodedToken)
     
 
     return{
@@ -60,8 +64,10 @@ export const handler: CustomAuthorizerHandler =async (event: CustomAuthorizerEve
 
   }
 }
+)
 
-async function verifyToken(authHeader: string): Promise<JwtToken> {
+
+function verifyToken(authHeader: string, secret: string): JwtToken {
 
   if (!authHeader) 
     throw new Error("No authorization header");   
@@ -72,19 +78,25 @@ async function verifyToken(authHeader: string): Promise<JwtToken> {
   const split = authHeader.split(' ')
   const token = split[1]
 
-  //logger.info('secret a secretID', secretId)
-  const secretObject: any =await getSecret()
-  const secret = secretObject[secretField]
-  //logger.info('secret a secretObject:', secretObject)
-  //logger.info('token a user', token)
-  return verify(token, secret) as JwtToken
-     
+  return verify(token, secret) as JwtToken    
 
 }
 
-async function getSecret(){
-  if(cachedSecret) return cachedSecret
 
+handler.use(
+  secretsManager({
+    cache: true,
+    cacheExpiryInMillis: 60000,
+    throwOnFailedCall: true,
+    secrets: {
+      AUTH0_SECRET: secretId
+    }
+  })
+)
+
+/*async function getSecret(){
+ // if(cachedSecret) return cachedSecret
+ logger.info('token a user', cachedSecret)
   const data = await client
         .getSecretValue({
           SecretId: secretId
@@ -95,83 +107,4 @@ async function getSecret(){
   logger.info('cachedSecret a user', cachedSecret)
 
   return JSON.parse(cachedSecret)
-}
-
-
-/*
-import { verify, decode } from 'jsonwebtoken'
-import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
-import { JwtPayload } from '../../auth/JwtPayload'
-
-const logger = createLogger('auth')
-
-TODO: Provide a URL that can be used to download a certificate that can be used
-to verify JWT token signature.
-To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
-
-export const handler = async (
-  event: CustomAuthorizerEvent
-): Promise<CustomAuthorizerResult> => {
-  logger.info('Authorizing a user', event.authorizationToken)
-  try {
-    const jwtToken = await verifyToken(event.authorizationToken)
-    logger.info('User was authorized', jwtToken)
-
-    return {
-      principalId: jwtToken.sub,
-      policyDocument: {
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Action: 'execute-api:Invoke',
-            Effect: 'Allow',
-            Resource: '*'
-          }
-        ]
-      }
-    }
-  } catch (e) {
-    logger.error('User not authorized', { error: e.message })
-
-    return {
-      principalId: 'user',
-      policyDocument: {
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Action: 'execute-api:Invoke',
-            Effect: 'Deny',
-            Resource: '*'
-          }
-        ]
-      }
-    }
-  }
-}
-
-async function verifyToken(authHeader: string): Promise<JwtPayload> {
-  const token = getToken(authHeader)
-  console.log(token)
-  //const jwt: Jwt = decode(token, { complete: true }) as Jwt
-
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
-}
-
-function getToken(authHeader: string): string {
-  if (!authHeader) throw new Error('No authentication header')
-
-  if (!authHeader.toLowerCase().startsWith('bearer '))
-    throw new Error('Invalid authentication header')
-
-  const split = authHeader.split(' ')
-  const token = split[1]
-
-  return token
-}
-*/
+}*/
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,19 +1,43 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 
 import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
 import { getUserId } from '../utils';
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('getTodoHandler')
 
 // TODO: Get all TODO items for a current user
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    // Write your code here
-    const todos = '...'
+    logger.info('event: ', event);
+  const userId = getUserId(event);
 
-    return undefined
+  const result = await getTodosForUser(userId);
+  logger.info('user: ', userId, 'todos: ', result.items);
+
+  if (Array.isArray(result.items) && result.items.length > 0) {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        items: result.items
+      })
+    }
+  }
+
+  return {
+    statusCode: 400,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: 'There is no todos found for this user'
+  }
+})
 
 handler.use(
   cors({
